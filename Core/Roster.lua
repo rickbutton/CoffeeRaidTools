5c47d9315020847688b22b9af0ffed8bf2b10faa
Rick Button <rick@button.dev>
2026-03-31 18:44:45 -0400

    add script to roster.json

--- Core/Roster.lua
+++ Core/Roster.lua
@@ -28,6 +28,7 @@ local Roster = {
     { nickname = "Peer", battletag = "notlad#11770" },
     { nickname = "Rocky", battletag = "bonestorm#11570" },
     { nickname = "Sad", battletag = "ophidian#1948" },
+    { nickname = "Script", battletag = "unlethal#1980" },
     { nickname = "Sicarius", battletag = "acheronfate#1797" },
     { nickname = "Sluff", battletag = "sluff#11368" },
     { nickname = "Squish", battletag = "squeethetree#1185" },
